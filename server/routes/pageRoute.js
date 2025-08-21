371d211c833f41c3cc7c10b09f5cebd14fdba648
THE CGEC TIMES <thecgectimesofficial@gmail.com>
2025-08-21 21:17:34 +0530

    added

--- server/routes/pageRoute.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const pageRouter=express.Router();
-const pageControll=require('../controller/pageControll');
-
-
-
-pageRouter.get('/',pageControll.getHome);
-
-module.exports= pageRouter;
